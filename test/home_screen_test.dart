724f869075dbe6df4c8f3932ec78ed3d62a9d733
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-04 13:54:42 -0500

    test home screen to have circular progress indicator first and then ListView

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -1,7 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:users_http_unit_test/home_screen.dart';
+import 'package:users_http_unit_test/user_model.dart';
 
 void main() {
-  testWidgets('home screen ...', (tester) async {
-    // TODO: Implement test
+  testWidgets(
+      'given home screen when material app opens home screen then displays List of user with title as name and subtitle as email',
+      (tester) async {
+    final users = [
+      User(
+          email: 'christiantsoungui@gmail.com',
+          name: 'Christian',
+          id: 1,
+          website: 'website'),
+      User(
+          email: 'kelliecorbett@gmail.com',
+          name: 'Kellie',
+          id: 2,
+          website: 'website2')
+    ];
+
+    Future<List<User>> mockFetchUsers() async {
+      return users;
+    }
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: HomeScreen(
+          futureUsers: mockFetchUsers(),
+        ),
+      ),
+    );
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    await tester.pump();
+    expect(find.byType(ListView), findsOneWidget);
   });
 }
